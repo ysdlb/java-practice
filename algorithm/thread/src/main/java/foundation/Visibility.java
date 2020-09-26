9bc84e0dc48540ecc4e3ee0138a52f4508d37b47
gqhan <gangqing.han@vova.com.hk>
2020-09-26 18:02:52 +0800

    u

---
+++ algorithm/thread/src/main/java/foundation/Visibility.java
@@ -0,0 +1,60 @@
+package foundation;
+
+public class Visibility {
+    public static void main(String[] args) throws InterruptedException {
+        TimeConsumingTask task = new TimeConsumingTask();
+        Thread thread = new Thread(task);
+
+        thread.start();
+
+        Thread.sleep(1000);
+
+        task.cancel();
+    }
+}
+class TimeConsumingTask implements Runnable {
+    private boolean toCancel = false;
+
+    @Override
+    public void run() {
+        while (!toCancel) {
+            if (doExecute()) {
+                break;
+            }
+        }
+        /**
+         * 上面代码被优化成了这样
+        if (!toCancel) {
+            while (true) {
+                if (doExecute()) {
+                    break;
+                }
+            }
+        }
+         */
+        if (toCancel) {
+            System.out.println("Task was canceled.");
+        } else {
+            System.out.println("Task done.");
+        }
+    }
+
+    private boolean doExecute() {
+        boolean isDone = false;
+        System.out.println("executing ...");
+
+        // 模拟实际操作的时间消耗
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+
+        }
+
+        return isDone;
+    }
+
+    public void cancel() {
+        toCancel = true;
+        System.out.println(this + " canceled");
+    }
+}
