f6eddfa6e4ffc78d58f0b51cc324f49d6bb770a3
gqhan <gangqing.han@vova.com.hk>
2020-11-19 20:02:52 +0800

    int symbol "_"

---
+++ property/foundation/src/main/java/ysdlb/foundation/type/IntMy.java
@@ -0,0 +1,10 @@
+package ysdlb.foundation.type;
+
+public class IntMy {
+    public static void main(String[] args) {
+        int i = 10_000;
+        int b = 1_1_1_1;
+        System.out.println(i);
+        System.out.println(b);
+    }
+}
