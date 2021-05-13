830155eeb89cff7e241164ec4d40e7c1d312b156
gqhan <libertyhan@outlook.com>
2021-05-13 19:49:52 +0800

    Java Object Layout start

---
+++ property/jmm-learn/src/main/java/ysdlb/learn/jmm/ObjectHeader.java
@@ -0,0 +1,27 @@
+package ysdlb.learn.jmm;
+
+import org.junit.jupiter.api.Test;
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+public class ObjectHeader {
+
+    @Test
+    void vm() {
+        System.out.println(VM.current().details());
+    }
+
+    @Test
+    void instanceObject() {
+        Object o = new Object();
+        String printable = ClassLayout.parseInstance(o).toPrintable();
+        System.out.println(printable);
+    }
+
+    @Test
+    void arrayInstance() {
+        int[] o = new int[]{};
+        String printable = ClassLayout.parseInstance(o).toPrintable();
+        System.out.println(printable);
+    }
+}
